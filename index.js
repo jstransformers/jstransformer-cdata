07af1302fa9bf8637db22954aa42b414814411ef
Timothy Gu <timothygu99@gmail.com>
2015-02-15 16:04:50 -0800

    Precompile regex

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var re = /\]\]>/g;
+
 exports.name = 'cdata';
 exports.outputFormat = 'xml';
 
 exports.render = function (str, options) {
-  var escaped = str.replace(/\]\]>/g, "]]]]><![CDATA[>");
+  var escaped = str.replace(re, "]]]]><![CDATA[>");
   return '<![CDATA[' + escaped + ']]>';
 }
